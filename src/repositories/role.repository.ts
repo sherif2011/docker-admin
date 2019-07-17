8927d174076a5cdd5c901f9461b4278d3d8abbe5
smankarious <smankarious@sunrisedataservices.com>
2019-07-17 03:51:23 -0400

    Fixing roles - string array issue

--- src/repositories/role.repository.ts
+++ src/repositories/role.repository.ts
@@ -3,6 +3,7 @@ import {inject} from '@loopback/core';
 import {PgdbDataSource} from '../datasources';
 import {Role} from '../models';
 import {DefaultSoftCrudRepository} from './default-soft-crud.repository.base';
+import {DataObject, Options} from '@loopback/repository';
 
 export class RoleRepository extends DefaultSoftCrudRepository<
   Role,
@@ -10,5 +11,47 @@ export class RoleRepository extends DefaultSoftCrudRepository<
 > {
   constructor(@inject('datasources.pgdb') dataSource: PgdbDataSource) {
     super(Role, dataSource);
+  }
+
+  async create(entity: DataObject<Role>, options?: Options): Promise<Role> {
+    //const role = await super.create(entity, options);
+    const permissions = `{${
+      entity.permissions ? entity.permissions.join() : ''
+    }}`;
+    const clients = `{${entity.clients ? entity.clients.join() : ''}}`;
+
+    const query = `INSERT INTO admin.roles(
+      permissions, clients, name, deleted, role_key)
+      VALUES ('${permissions}', '${clients}', '${entity.name}', '${
+      entity.deleted
+    }', '${entity.roleKey}')`;
+    await super.execute(query, []);
+    return (await this.findOne({
+      order: ['createdOn DESC'],
+    })) as Role;
+  }
+
+  async updateById(id: number, entity: DataObject<Role>): Promise<void> {
+    const permissions = `{${
+      entity.permissions ? entity.permissions.join() : ''
+    }}`;
+    const clients = `{${entity.clients ? entity.clients.join() : ''}}`;
+
+    const query = `UPDATE admin.roles SET
+     permissions='${permissions}', clients= '${clients}', name='${entity.name}',
+     deleted='${entity.deleted}',role_key='${entity.roleKey}' WHERE id=${id}`;
+    await super.execute(query, []);
+  }
+
+  async replaceById(id: number, entity: DataObject<Role>): Promise<void> {
+    const permissions = `{${
+      entity.permissions ? entity.permissions.join() : ''
+    }}`;
+    const clients = `{${entity.clients ? entity.clients.join() : ''}}`;
+
+    const query = `UPDATE admin.roles SET
+     permissions='${permissions}', clients= '${clients}', name='${entity.name}',
+     deleted='${entity.deleted}',role_key='${entity.roleKey}' WHERE id=${id}`;
+    await super.execute(query, []);
   }
 }

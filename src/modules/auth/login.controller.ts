9b3dc8121453323c027c12e689b5240d13d2b38d
smankarious <smankarious@sunrisedataservices.com>
2019-07-18 15:18:01 -0400

    fix exclusion of clients

--- src/modules/auth/login.controller.ts
+++ src/modules/auth/login.controller.ts
@@ -396,7 +396,12 @@ export class LoginController {
       });
       utClients.forEach(a => {
         console.log(a);
-        group.clients.push(a.client);
+        if (a.allowed === true) group.clients.push(a.client);
+        else {
+          let idx = group.clients.indexOf(a.client, 0);
+          if (idx >= 0) group.clients.splice(idx, 1);
+          console.log(group.clients);
+        }
       });
       authUser.clients = group.clients;
       authUser.permissions = this.getUserPermissions(utPerms, role.permissions);
